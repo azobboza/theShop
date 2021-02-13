e60f4a1263428101a5916a0e0f721d29b2b152c9
Bozidar Trifunovic <bozidar.trifunovic85@gmail.com>
2021-02-13 21:52:21 +0100

    Introduce IDatabaseDriver interface

---
+++ TheShop/Persistance/DatabaseDriver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Contracts;
+using TheShop.Entities;
+
+namespace TheShop.Persistance
+{
+    public class DatabaseDriver : IDatabaseDriver
+    {
+        private readonly List<Article> _articles;
+        public DatabaseDriver()
+        {
+            _articles = new List<Article>();
+        }
+        public Article GetById(int id)
+        {
+            return _articles.Single(x => x.Id == id);
+        }
+
+        public void Save(Article article)
+        {
+            _articles.Add(article);
+        }
+    }
+}
