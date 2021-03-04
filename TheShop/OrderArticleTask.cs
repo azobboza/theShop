21ebd35dd576258b271cce7299c2ef936424f6f0
Bozidar Trifunovic <bozidar.trifunovic85@gmail.com>
2021-03-04 13:30:26 +0100

    refactor orderArticle

--- TheShop/OrderArticleTask.cs
+++ TheShop/OrderArticleTask.cs
@@ -14,8 +14,8 @@ namespace TheShop
         public int ArticleId { get; private set; }
         public double MaxExpectedPrice { get; private set; }
         public int BuyerId { get; private set; }
-        private readonly List<ISupplier> _suppliers;
-        public OrderArticleTask(int articleId, double maxExpectedPrice, int buyerId, List<ISupplier> suppliers)
+        private readonly IEnumerable<ISupplier> _suppliers;
+        public OrderArticleTask(int articleId, double maxExpectedPrice, int buyerId, IEnumerable<ISupplier> suppliers)
         {
             ArticleId = articleId;
             MaxExpectedPrice = maxExpectedPrice;
@@ -24,18 +24,20 @@ namespace TheShop
         }
         public void Execute()
         {
-            foreach (var supplier in _suppliers)
-            {
-                var article = supplier.GetArticle(ArticleId);
-                if (article != null)
-                {
-                    if (article.Price < MaxExpectedPrice)
-                    {
-                        InMemoryDatabase.Save(new Article { Id = ArticleId, IsSold = true, SoldDate = DateTime.Now, BuyerId = BuyerId });
-                        return;
-                    }
-                }
-            }
+            var article = _suppliers.Select(s => s.GetArticle(ArticleId)).Where(a => a != null && a.Price < MaxExpectedPrice);
+
+            //foreach (var supplier in _suppliers)
+            //{
+            //    var article = supplier.GetArticle(ArticleId);
+            //    if (article != null)
+            //    {
+            //        if (article.Price < MaxExpectedPrice)
+            //        {
+            //            InMemoryDatabase.Save(new Article { Id = ArticleId, IsSold = true, SoldDate = DateTime.Now, BuyerId = BuyerId });
+            //            return;
+            //        }
+            //    }
+            //}
         }
     }
 }
